da113f0c719c2a1373638880d715ff3de005476b
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-02-16 18:20:46 -0500

    added doc for multi-line strings

---
+++ strings.py
@@ -0,0 +1,11 @@
+print('Strings:')
+
+print('You can use \"\" and \'\'  ')
+
+print('You can also create multi-line strings with: \n \'\'\'...\'\'\' and \n \"\"\"...\"\"\"')
+
+print(''' example:
+    first line
+    second line
+    third line
+''')
