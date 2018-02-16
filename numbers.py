ea698126b946a29961e834105f1c483e3f3f1e12
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-02-16 14:48:53 -0500

    adding simple numeric operations

---
+++ numbers.py
@@ -0,0 +1,17 @@
+print('2 + 2 =',2 + 2)
+
+print('50 - 5*6 =', 50 - 5*6)
+
+print('(50 - 5*6) / 4 =', (50 - 5*6) / 4)
+
+print('8/5 =', 8/5)
+
+print('division always returns a foalting point number')
+
+print('The integer numbers (e.g. 2, 4, 20) have type int, the ones with a fractional part (e.g. 5.0, 1.6) have type float.')
+
+print('Use // to return an int instead of float when dividing numbers. (fractional result will be disgarded)')
+
+print('(50 - 5*6) // 4 now retuns:')
+
+print((50 - 5*6) // 4)
